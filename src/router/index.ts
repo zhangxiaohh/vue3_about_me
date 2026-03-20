91f423eb1fe8f645bfd4447c88057991d03a6a66
vento <210567852@qq.com>
2026-03-20 20:13:25 +0800

    deploy pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Projects from '../views/Projects.vue'
 import Experience from '../views/Experience.vue'
@@ -6,7 +6,7 @@ import Contact from '../views/Contact.vue'
 import Details from '../views/Details.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
